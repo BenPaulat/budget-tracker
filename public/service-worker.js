316c4187fc1246af2155c2b23b97ae06f255b5a8
BenPaulat <benjamin.d.paulat@gmail.com>
2022-01-09 16:41:53 -0600

    added activate event listenter to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -25,3 +25,21 @@ self.addEventListener('install', function (e) {
         })
     )
 })
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.add(keyList.map(function (key, i) {
+                if (cacheKeepList.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            }));
+        })
+    );
+});
